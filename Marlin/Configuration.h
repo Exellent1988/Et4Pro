31ee7c41cfb2dd52877efaf4bfb7f348c53546dd
Exellent <axel@dawolke.de>
2021-10-26 20:55:13 +0200

    BTR Extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -935,7 +935,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.4, 80.4, 403.1, 100.7 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.4, 80.4, 403.1, 382.165 }
 
 /**
  * Default Max Feed Rate (mm/s)
